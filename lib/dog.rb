f9e56afd36c00e7feece1f97c9fdbdf19716c652
Brandon Green <brandon@brandonleeproductions.com>
2018-08-16 23:38:05 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,12 @@
 class Dog
   attr_accessor :id, :name, :breed
-
+  @@all = []
 
   def initialize(id: nil, name:, breed:)
     @id = id
     @name = name
     @breed = breed
+    @@all << self
   end
 
   def self.create_table
