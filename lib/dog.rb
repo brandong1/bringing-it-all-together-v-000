b5aae29984901333ed01afea9d7203f11880ce81
Brandon Green <brandon@brandonleeproductions.com>
2018-08-17 00:16:02 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -35,12 +35,11 @@ class Dog
           INSERT INTO dogs (name, breed)
           VALUES (?, ?)
         SQL
-
-    DB[:conn].execute(sql, self.name, self.breed)
-
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+        DB[:conn].execute(sql, self.name, self.breed)
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
     #binding.pry
       end
+      self
     end
 
     def self.create(name, breed)
